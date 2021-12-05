25086aa5e4aa3813cfdc125148a4c46dcf425aaa
mahdizmni <mahdizmni@yahoo.com>
2021-12-04 20:14:09 -0500

    ActivityManageOrder class and layout initial release

--- app/src/main/java/com/example/gproject/ManageOrderActivity.java
+++ app/src/main/java/com/example/gproject/ManageOrderActivity.java
@@ -1,21 +1,65 @@
 package com.example.gproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
-import com.example.gproject.R;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class ManageOrderActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    DatabaseReference ref;
+    ArrayList<Order> orders;
+    ManageOrderAdapter adapter;
+    String username;
 
     public void goBack(View v) {
         onBackPressed();
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_order);
+        Intent intent = getIntent();
+        username = intent.getStringExtra(OwnerMainActivity.EXTRA_MESSAGE);
+
+        recyclerView = findViewById(R.id.list_item_order);
+        ref = FirebaseDatabase.getInstance().getReference().child("owners").child(username).child("customer_order");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        orders = new ArrayList<>();
+        adapter = new ManageOrderAdapter(this, orders);
+        recyclerView.setAdapter(adapter);
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds: snapshot.getChildren())
+                    orders.add(ds.getValue(Order.class));
+
+                adapter = new ManageOrderAdapter(getApplicationContext(), orders);
+                recyclerView.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/gproject/ManageOrderAdapter.java
@@ -0,0 +1,59 @@
+package com.example.gproject;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ManageOrderAdapter extends RecyclerView.Adapter<ManageOrderAdapter.ViewHolder> {
+
+    Context context;
+    ArrayList<Order> order_list;
+
+
+    public ManageOrderAdapter(Context context, ArrayList<Order> order_list) {
+        this.context = context;
+        this.order_list = order_list;
+    }
+
+
+    @NonNull
+    @Override
+    public ManageOrderAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.order_items,parent,false);
+        return new ManageOrderAdapter.ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        Order order = order_list.get(position);
+//        String str = list.get(position);
+        holder.customer_name.setText(order.customer);
+        holder.total_price.setText(String.valueOf(order.final_price));
+
+    }
+    @Override
+    public int getItemCount() {
+        return order_list.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView customer_name, total_price;
+        Button view_order, complete;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            customer_name = itemView.findViewById(R.id.customer_name);
+            total_price = itemView.findViewById(R.id.total_price);
+            view_order = itemView.findViewById(R.id.view_order);
+            complete = itemView.findViewById(R.id.complete);
+        }
+    }
+}
